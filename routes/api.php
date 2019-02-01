416599949c11557caa7aa08bc6346c6694e3b7fc
raylison100 <raylisonnunes@mangue3.com>
2019-02-01 09:01:39 -0300

    Rotas da API

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +11,37 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function () {
+    return redirect('/');
 });
+////Authentication Route
+//Route::post('authentication','\Laravel\Passport\Http\Controllers\AccessTokenController@issueToken');
+//
+//Route::post('password/email',   'UsersController@forgotPassword');
+//Route::post('password/reset',   'UsersController@confirmPasswordReset');
+//Route::post('confirm-register', 'UsersController@confirmSignUp');
+//
+////Pre-Providers
+//Route::post('check-token',       'PreProvidersController@checkToken');
+//Route::post('provider-register', 'ProvidersController@store');
+//
+//Route::group(['middleware' => ['auth.api']], function () {
+//
+//    //Users
+//    Route::get('user-authenticated',     'UsersController@getUserAuthenticated');
+//
+//    //Providers
+//    Route::get('provider-quotations',    'QuotationsController@listQuotationsByProvider');
+//    Route::put('provider-update',        'ProvidersController@update');
+//    Route::get('/provider',               'ProvidersController@getProviderData');
+//
+//    //Banks
+//    Route::get('banks',                  'BanksController@listAll');
+//    Route::get('segments/{bank_id}',     'SegmentsController@listByBank');
+//
+//    //Programs
+//    Route::get('programs',               'ProgramsController@index');
+//
+//    //Orders
+//    Route::resource('orders',   'OrdersController', ['except' => ['edit', 'delete', 'index']]);
+//});
